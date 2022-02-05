ce50b2f56235e933bf5fbbc85efdcd012c11737c
Landon Deam <66180519+LandonDeam@users.noreply.github.com>
2022-02-05 17:45:37 -0500

    added procfile

--- skot-botagami/Program.cs
+++ skot-botagami/Program.cs
@@ -37,7 +37,7 @@ namespace skot_botagami
                 .AddSingleton(_commands)
                 .BuildServiceProvider();
 
-            //string token = _config["token"];
+            //string token = _config["token"]; // use this line instead of the one under it to run the bot locally
             string token = Environment.GetEnvironmentVariable("token");
 
             _client.Log += _client_Log;
