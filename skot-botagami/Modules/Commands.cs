ffbfcb72a9cda4c4e0b35a661e33d322e4c402c9
Landon Deam <66180519+LandonDeam@users.noreply.github.com>
2022-07-28 02:18:50 -0400

    more aestetic fixes

--- skot-botagami/Modules/Commands.cs
+++ skot-botagami/Modules/Commands.cs
@@ -21,14 +21,15 @@ namespace skot_botagami.Modules
         {
             Blackjack game = new Blackjack();
             game.deal();
-            EmbedBuilder embed = new EmbedBuilder
-            {
-                Title = "Blackjack",
-                Description = $"Dealer first: {game.getDealerFirst()}\n{game.playerHand()}?"
-            };
-            embed.AddField($"Dealer face up ({game.dealerHandValue()})", game.getDealerFirst());
-            embed.AddField($"Player hand ({game.playerHandValue()})", game.playerHand());
-            embed.WithAuthor(author: new EmbedAuthorBuilder {Name=Context.Client.CurrentUser.Username, IconUrl= "https://media.discordapp.net/attachments/1000464257181286463/1002092083873587200/Cowboy_Bebop_Ed_Hacking_Logo_No_BG.png" });
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.WithTitle("Blackjack")
+                .AddField($"Dealer face up ({game.dealerHandValue()})", game.getDealerFirst())
+                .AddField($"Player hand ({game.playerHandValue()})", game.playerHand())
+                .WithAuthor(author: new EmbedAuthorBuilder
+                {
+                    Name = Context.Client.CurrentUser.Username,
+                    IconUrl = "https://media.discordapp.net/attachments/1000464257181286463/1002092083873587200/Cowboy_Bebop_Ed_Hacking_Logo_No_BG.png"
+                });
             Emoji hit = new Emoji(":HIT:1002084815023124541");
             Emoji stand = new Emoji(":STAND:1002085670594035772");
             IMessage msg = await Context.Channel.SendMessageAsync(embed: embed.Build());
