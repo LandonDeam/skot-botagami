3fd608d0702773228400e0baf85577afcd533812
Landon Deam <66180519+LandonDeam@users.noreply.github.com>
2022-07-28 03:13:07 -0400

    testing reaction event handler

--- skot-botagami/Event Handlers/ReactionEventHandler.cs
+++ skot-botagami/Event Handlers/ReactionEventHandler.cs
@@ -10,6 +10,7 @@ public class ReactionEventHandler
 {
     public static Task ReactionAdded(Cacheable<IUserMessage, ulong> message, Cacheable<IMessageChannel, ulong> channel, SocketReaction reaction)
     {
+        Console.WriteLine($"{message.Id} was reacted to with {reaction.Emote.Name} by {reaction.User.Value}");
         return Task.CompletedTask;
     }
 }
